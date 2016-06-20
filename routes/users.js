4f833ba7739f3d76aa9ad79e132cc2f837c89310
Cassiano de Brito Andrade <cassiano.b.andrade@gmail.com>
2016-06-20 01:54:46 -0300

    Back-end: Implementando lógica do get (validação login)

--- routes/users.js
+++ routes/users.js
@@ -5,18 +5,27 @@ var router = express.Router();
 
 /* GET lista de pessoas. */
 router.get('/', function(req, res, next) {
-    db.query({
-        sql: 'SELECT * FROM user',
-        nestTables: true
+  db.query({
+    sql: 'SELECT * FROM user WHERE user.email = req.body.email',
+    nestTables: true
     }, function(err, rows) {
-        if (err) res.status(500).send('Problema ao recuperar usuários.');
+      if (err) res.status(500).send('Problema ao recuperar usuários.');
 
-      /*res.render('listUsers', {
-        users: rows,
-        success: req.flash('success'),
+      if (rows.length == 1) { //Sucesso! Email cadastrado!
+        if (rows[0].password == req.body.password) { //Password certa! \o/
+          res.render('/home/', { //Home ainda não foi implementada no app.js
+            users: rows,
+            success: req.flash('success'),
+          });
+        } else {
+          res.render('/', { //Senha errada! Volta para a página de login.
+            error: req.flash('error')
+          });
+        }
+      } else { //Ops! Email não cadastrado
         error: req.flash('error')
-      });*/
-    });
+      }
+  });
 });
 
 /* POST registra uma nova pessoa */
@@ -31,7 +40,7 @@ router.post('/', function(req, res) {
     db.query('INSERT INTO user (nickname, email, password, pictureUrl) VALUES (' + username + ',' + email + ',' + password + ',' + pictureUrl + ')',
     /*db.query('INSERT INTO user (nickname, email, password, pictureUrl) VALUES ("Vilma", "vilma@uol.com.br", "987", null)',*/
     /*db.query('SELECT * FROM user',*/
-    
+
         function(err, result) {
             if (err) { res.send(404, 'It wasnt possible to add an user=('); }
             else { res.send(404, 'It was possible to add an user \o/');/*res.redirect('#/login/');*/ }
