c3db4110d4b1e113e403337d03e8a296e34587d1
Cassiano de Brito Andrade <cassiano.b.andrade@gmail.com>
2016-06-20 02:00:24 -0300

    Back-end: Consertando posts duplos

--- routes/users.js
+++ routes/users.js
@@ -30,21 +30,17 @@ router.get('/', function(req, res, next) {
 
 /* POST registra uma nova pessoa */
 router.post('/', function(req, res) {
-    var username = db.escape(req.body.username);
-    var email = db.escape(req.body.email);
-    var password = db.escape(req.body.password);
-    //var pictureUrl = db.escape(req.body.pictureUrl);
-    var pictureUrl = null;
-    //console.log(nickname + " " + email + " " + password);
+  var username = db.escape(req.body.username);
+  var email = db.escape(req.body.email);
+  var password = db.escape(req.body.password);
+  //var pictureUrl = db.escape(req.body.pictureUrl);
+  var pictureUrl = null;
 
-    db.query('INSERT INTO user (nickname, email, password, pictureUrl) VALUES (' + username + ',' + email + ',' + password + ',' + pictureUrl + ')',
-    /*db.query('INSERT INTO user (nickname, email, password, pictureUrl) VALUES ("Vilma", "vilma@uol.com.br", "987", null)',*/
-    /*db.query('SELECT * FROM user',*/
-
-        function(err, result) {
-            if (err) { res.send(404, 'It wasnt possible to add an user=('); }
-            else { res.send(404, 'It was possible to add an user \o/');/*res.redirect('#/login/');*/ }
-    })
+  db.query('INSERT INTO user (nickname, email, password, pictureUrl) VALUES (' + username + ',' + email + ',' + password + ',' + pictureUrl + ')',
+    function(err, result) {
+      if (err) { res.send(404, 'It wasnt possible to add an user=('); }
+      else { res.redirect('/'); }
+  })
 });
 
 module.exports = router;
