e6a1b9e9b5ecb5879c383a2423f45f874b9ea73e
Cassiano de Brito Andrade <cassiano.b.andrade@gmail.com>
2016-06-20 02:34:15 -0300

    Back-end: Implementando get (validação login)

--- routes/users.js
+++ routes/users.js
@@ -5,27 +5,45 @@ var router = express.Router();
 
 /* GET lista de pessoas. */
 router.get('/', function(req, res, next) {
-  db.query({
-    sql: 'SELECT * FROM user WHERE user.email = req.body.email',
+  var email = db.escape(req.body.email);
+  var password = db.escape(req.body.password);
+
+  /*db.query('SELECT * FROM user WHERE user.email = ' + email*/
+  db.query('SELECT * FROM user WHERE user.email = "iggyazalea@gmail.com"',
+    function(err, rows) {
+      if (err) { res.send(404, 'Couldnt get user'); }
+      else {
+        /*res.redirect('/');*/
+        if (rows.length == 1)
+        {
+          if (rows[0].password = password) {
+              res.send(404, 'Correct password!');
+          } else {
+              res.send(404, 'Incorrect password!');
+          }
+        } else {
+          res.send(404, 'Email not registered');
+        }
+      }
+  })
+
+  /*db.query({
+    sql: ('SELECT * FROM user WHERE user.email = ' + req.body.email),
     nestTables: true
     }, function(err, rows) {
       if (err) res.status(500).send('Problema ao recuperar usuários.');
 
-      if (rows.length == 1) { //Sucesso! Email cadastrado!
-        if (rows[0].password == req.body.password) { //Password certa! \o/
-          res.render('/home/', { //Home ainda não foi implementada no app.js
-            users: rows,
-            success: req.flash('success'),
-          });
-        } else {
-          res.render('/', { //Senha errada! Volta para a página de login.
-            error: req.flash('error')
-          });
-        }
-      } else { //Ops! Email não cadastrado
-        error: req.flash('error')
+      else if (rows.length == 1) { //Sucesso! Email cadastrado!
+          if (rows[0].password == req.body.password) { //Password certa! \o/
+              res.send(404, 'SUCCESS \O/');
+
+          } else {
+            res.send(404, 'Senha errada :B');
+            }
+       } else { //Ops! Email não cadastrado
+          res.send(404, 'Este email está errado! O.o');
       }
-  });
+  });*/
 });
 
 /* POST registra uma nova pessoa */
