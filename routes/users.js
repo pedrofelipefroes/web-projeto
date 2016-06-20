f95f546fb59fe7a0e948e3d02fe164dd52bd6157
Cassiano de Brito Andrade <cassiano.b.andrade@gmail.com>
2016-06-20 19:23:34 -0300

    Back-end: Conclusão dos métodos login/signup de user

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ var router = express.Router();
 /* POST logar pessoa. */
 router.post('/login/', function(req, res, next) {
   var email = db.escape(req.body.email);
-  var password = db.escape(req.body.password);
+  var password = (db.escape(req.body.password)).replace(/['"]+/g, '');
   var sql = 'SELECT * FROM user WHERE email = ' + email;
   //res.send(404,sql);
   db.query(sql,
@@ -17,38 +17,15 @@ router.post('/login/', function(req, res, next) {
         //res.redirect('/');
         if (rows.length == 1)
         {
-          if (rows[0].password == password) {
-              res.send(404, "Certa: " + rows[0].password + " " + password);
-          } else {
-            res.send(404, "Errada: " + rows[0].password + " " + password);
-          }
-        } else {
-          res.send(404, 'Não existe: ' + rows.length + " " + password);
-        }
+          if (rows[0].password == password) { res.send(404, "Acertou a senha mas o Pedro ainda não colocou a página =)");
+          } else { res.redirect('/'); }
+        } else { res.redirect('/'); }
       }
   })
-
-  /*db.query({
-    sql: ('SELECT * FROM user WHERE user.email = ' + req.body.email),
-    nestTables: true
-    }, function(err, rows) {
-      if (err) res.status(500).send('Problema ao recuperar usuários.');
-
-      else if (rows.length == 1) { //Sucesso! Email cadastrado!
-          if (rows[0].password == req.body.password) { //Password certa! \o/
-              res.send(404, 'SUCCESS \O/');
-
-          } else {
-            res.send(404, 'Senha errada :B');
-            }
-       } else { //Ops! Email não cadastrado
-          res.send(404, 'Este email está errado! O.o');
-      }
-  });*/
 });
 
-/* POST registra uma nova pessoa */
-/*router.post('/signup/', function(req, res) {
+//POST registra uma nova pessoa
+router.post('/signup/', function(req, res) {
   var username = db.escape(req.body.username);
   var email = db.escape(req.body.email);
   var password = db.escape(req.body.password);
@@ -60,6 +37,6 @@ router.post('/login/', function(req, res, next) {
       if (err) { res.send(404, 'It wasnt possible to add an user=('); }
       else { res.redirect('/'); }
   })
-});*/
+});
 
 module.exports = router;
