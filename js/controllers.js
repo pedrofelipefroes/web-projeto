6dc82d4e091898c2b845254ec0fe87042fa27fc7
pedrofelipefroes <pedrofelipefroes@gmail.com>
2016-06-21 03:05:57 -0300

    Changes in Login Home Signup

--- js/controllers.js
+++ js/controllers.js
@@ -1,43 +1,61 @@
-app.controller('signinCtrl', ['$http', '$scope', '$state', 'userService', function($http, $scope, $state, userService) {
-    $scope.user = {};
+app.controller('signinCtrl', ['$scope', function($scope) {
+  $scope.character = [
+        { name: soraka, id: '#soraka', color: "#F384CA", duration: 25100 },
+        { name: amumu, id: '#amumu', color: "#00D8CB", duration: 17400 },
+        { name: fiora, id: '#fiora', color: "#FF5656", duration: 15600 },
+        { name: ahri, id: '#ahri', color: "#89BDEF", duration: 29200 },
+        { name: annie, id: '#annie', color: "#FF5656", duration: 14700 },
+        { name: janna, id: '#janna', color: "#89BDEF", duration: 23600 },
+    ];
 
-    $scope.userLogin = function () {
-        $http.post('url/do/post', {
-            email: $scope.user.email,
-            password: $scope.user.password
-        }).success(function (data) {
-            userService.setUser(data.username, data.email);
-            $state.go('/home');
-        });
+    var draw = function (it) {
+        $($scope.character[it].id).removeClass("invisible");
+        $($scope.character[it].id).add("invisible");
+
+        $($scope.character[it].id).lazylinepainter({
+            "svgData": $scope.character[it].name,
+            "strokeWidth": 1.5,
+            "strokeColor": $scope.character[it].color,
+            "onComplete": function () {
+                $($scope.character[it].id).addClass("invisible");
+                $($scope.character[it].id).removeClass("visible");
+
+                if (it == 5) it = it - 5;
+                else it = it + 1;
+
+                draw(it);
+            }
+        }).lazylinepainter('paint');
     }
 
-//    userService.addUser($scope.newuser.username, $scope.newuser.password);
+    draw(0);
+  
+//  $scope.user = {};
+//  
+//  console.log('yey!');
 
-    $(document).ready(function () {
-        $('#amumu').lazylinepainter({
-            "svgData": amumu,
-            "strokeWidth": 1.5,
-            "strokeColor": "#00D8CB"
-        }).lazylinepainter('paint');
-    });
+//  $('#amumu-forgot').lazylinepainter({
+//    "svgData": amumu,
+//    "strokeWidth": 1.5,
+//    "strokeColor": "#00D8CB"
+//  }).lazylinepainter('paint');
+  
+//  $('#annie-login')
+//
+//  $('#annie-login').lazylinepainter({
+//    "svgData": annie,
+//    "strokeWidth": 1.5,
+//    "strokeColor": "#FF5656"
+//  }).lazylinepainter('paint');
 
-    $(document).ready(function () {
-        $('#annie').lazylinepainter({
-            "svgData": annie,
-            "strokeWidth": 1.5,
-            "strokeColor": "#FF5656"
-        }).lazylinepainter('paint');
-    });
-
-    $(document).ready(function () {
-        $('#janna').lazylinepainter({
-            "svgData": janna,
-            "strokeWidth": 1.5,
-            "strokeColor": "#89BDEF"
-        }).lazylinepainter('paint');
-    });
+//$(document).ready(function () {
+//  $('#janna-signup').lazylinepainter({
+//    "svgData": janna,
+//    "strokeWidth": 1.5,
+//    "strokeColor": "#89BDEF"
+//  }).lazylinepainter('paint');
+//});
 }]);
-
 
 app.controller('homeCtrl', ['$scope', 'userService', function($scope, userService) {
 
